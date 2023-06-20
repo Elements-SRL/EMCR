07eb90405b0f9f9e0727d984f4df57267735a0b4
Luca Rossi <lucarossi147@gmail.com>
2023-06-20 11:22:28 +0200

    controllerchannel now uses lambda internally

--- src/controller/controllerchannel.cpp
+++ src/controller/controllerchannel.cpp
@@ -7,15 +7,33 @@ ControllerChannel::ControllerChannel(ModelDevice * mDev, MainWindow * mainWindow
 {
     channelControlsDw = new ChannelControlDockWidget(mDev);
     this->mainWindow = mainWindow;
-    connect(channelControlsDw, &ChannelControlDockWidget::sigAppliedTurnChannelOnOff,       this, &ControllerChannel::onApplyTurnChannelOnOff);
-    connect(channelControlsDw, &ChannelControlDockWidget::sigAppliedTurnStimulsOnOff,       this, &ControllerChannel::onApplyTurnStimulusOnOff);
-    connect(channelControlsDw, &ChannelControlDockWidget::sigAppliedTurnDocOnOff,           this, &ControllerChannel::onApplyTurnDocOnOff);
-    connect(channelControlsDw, &ChannelControlDockWidget::sigAppliedHoldValues,             this, &ControllerChannel::onApplyHoldValues);
-    connect(mainWindow->getChessaboard(), &Chessboard::allChannelsClicked,                  this, &ControllerChannel::onAllChannelsClicked);
-    connect(mainWindow->getChessaboard(), &Chessboard::oneRowClicked,                       this, &ControllerChannel::onOneRowClicked);
-    connect(mainWindow->getChessaboard(), &Chessboard::oneBoardClicked,                     this, &ControllerChannel::onOneBoardClicked);
-    connect(mainWindow->getChessaboard(), &Chessboard::singleChannelClicked,                this, &ControllerChannel::onSingleChannelClicked);
-    connect(mainWindow->getCompensationControlsDockWidget(), &CompensationControlDockWidget::sigCompensationsApplied,    this, &ControllerChannel::onCompensationApplied);
+    connect(channelControlsDw, &ChannelControlDockWidget::sigAppliedTurnChannelOnOff, this, [=](std::vector<uint16_t> channelIndexes, std::vector<bool> onValues){
+        onApplyTurnChannelOnOff(channelIndexes, onValues);
+    });
+    connect(channelControlsDw, &ChannelControlDockWidget::sigAppliedTurnStimulsOnOff, this, [=](std::vector<uint16_t> channelIndexes, std::vector<bool> onValues){
+        onApplyTurnStimulusOnOff(channelIndexes, onValues);
+    });
+    connect(channelControlsDw, &ChannelControlDockWidget::sigAppliedTurnDocOnOff, this, [=](std::vector<uint16_t> channelIndexes, std::vector<bool> onValues){
+        onApplyTurnDocOnOff(channelIndexes, onValues);
+    });
+    connect(channelControlsDw, &ChannelControlDockWidget::sigAppliedHoldValues, this, [=](std::vector<uint16_t> channelIndexes, std::vector<Measurement_t> holdValues){
+        onApplyHoldValues(channelIndexes, holdValues);
+    });
+    connect(mainWindow->getChessaboard(), &Chessboard::allChannelsClicked, this, [=](bool newChannelState){
+        onAllChannelsClicked(newChannelState);
+    });
+    connect(mainWindow->getChessaboard(), &Chessboard::oneRowClicked, this, [=](uint16_t changedRowIndex, bool newChannelState){
+        onOneRowClicked(changedRowIndex, newChannelState);
+    });
+    connect(mainWindow->getChessaboard(), &Chessboard::oneBoardClicked, this, [=](uint16_t changedBoardIndex, bool newChannelState){
+        onOneBoardClicked(changedBoardIndex, newChannelState);
+    });
+    connect(mainWindow->getChessaboard(), &Chessboard::singleChannelClicked, this, [=](uint16_t changedChannelIndexes, bool newChannelState){
+        onSingleChannelClicked(changedChannelIndexes, newChannelState);
+    });
+    connect(mainWindow->getCompensationControlsDockWidget(), &CompensationControlDockWidget::sigCompensationsApplied, this, [=](std::vector<uint16_t> channelIndexes, std::vector<bool> cfastEn, std::vector<bool> cslowRsEn, std::vector<bool> rsCpEn, std::vector<bool> rsPgEn, std::vector<double> cfastValues, std::vector<double> cslowValues, std::vector<double> rsValues, std::vector<double> rsCpValues, std::vector<double> rsPgValues, std::vector<uint16_t> rsBWValueIdxs, std::vector<bool> ccCfastEn, std::vector<double> ccCfastValues){
+        onCompensationApplied(channelIndexes, cfastEn, cslowRsEn, rsCpEn, rsPgEn, cfastValues, cslowValues, rsValues, rsCpValues, rsPgValues, rsBWValueIdxs, ccCfastEn, ccCfastValues);
+    });
     mainWindow->setChannelControlsDw(channelControlsDw);
 }
 
