53b6f8e0f689c5467652ae4e201d1c53769cf33d
fcona <fcona@elements-ic.com>
2024-05-29 17:26:28 +0200

    Fixed short recordings

--- src/controller/device/consumer/datawriterconsumer.cpp
+++ src/controller/device/consumer/datawriterconsumer.cpp
@@ -228,8 +228,8 @@ void DataWriterConsumer::computeSamples() {
     }
 
     /*! First samples computation, might not be the final one because of specific behaviorus when durations are 0 */
-    samplesToBeSaved = (long long) settings.recordDurationS*samplingRateHz;
-    samplesPerChunk = (long long) settings.chunkDurationS*samplingRateHz;
+    samplesToBeSaved = (long long) (settings.recordDurationS*samplingRateHz);
+    samplesPerChunk = (long long) (settings.chunkDurationS*samplingRateHz);
 
     unlimitedFlag = settings.recordDurationS == 0.0;
     chunkFlag = settings.chunkDurationS > 0.0;
