69b5953d37a2472b4dfcaf28b9c90cb99e02d06f
fcona <fcona@elements-ic.com>
2025-05-22 08:21:27 +0200

    first implementation

--- src/controller/temperaturecontroller.h
+++ src/controller/temperaturecontroller.h
@@ -33,8 +33,43 @@ private:
     const double ig = -0.5;
     const double ieMax = 1.0;
     double maxFanSpeed = 1.0;
-    const double minRT = 0.35;
+    const double minFanSpeed = 3524.0;
     Measurement_t speedSet;
+
+    /*! kalman */
+    const double dt = 2.0;
+    const double Rh = 0.023389878788146;
+    const double Cb = 51.709799818984635;
+    const double Ca = 5.154520605431273e+02;
+    const double offRT = 1.829506286371586;
+    const double maxRT = 0.335107062018345;
+    const double minRT = 0.239087355650064;
+    const double powerOut = 50.0;
+    const double sR2 = 0.25*0.25/12;
+    const double sQ12 = sR2/20.0;
+    const double sQ22 = sR2/20.0;
+
+    const double F11 = 1.0-dt/(Rh*Cb);
+    const double F12 = dt/(Rh*Cb);
+    const double F21 = dt/(Rh*Ca);
+    double F22p = 1.0-dt/(Rh*Ca);
+    double F22 = 0.0;
+    const double B11 = dt/Cb;
+    double B22p = 0.0;
+    double B22 = 0.0;
+    double Rf = 0.0;
+
+    double x1 = 0.0;
+    double x2 = 0.0;
+    double Pk11 = 0.0;
+    double Pk12 = 0.0;
+    double Pk21 = 0.0;
+    double Pk22 = 0.0;
+    double y = 0.0;
+    double S = 0.0;
+    double K1 = 0.0;
+    double K2 = 0.0;
+    double w = 0.0;
 };
 
 #endif // TEMPERATURECONTROLLER_H
