88235d561da3b7d9384523ded11f813b73629560
Luca Rossi <lucarossi147@gmail.com>
2025-06-04 08:42:08 +0200

    fix: bug fix forauto auto expand button

--- src/model/multiplechannelmodel.cpp
+++ src/model/multiplechannelmodel.cpp
@@ -51,12 +51,15 @@ void MultipleChannelModel::saveSettings() {
 void MultipleChannelModel::loadSettings() {
     QSettings settings;
     auto channelsAuto = settings.value(GLB_CONTROLS_CHANNEL_AUTO_TAG).toBool();
+    appStatus->setStimulusAuto(channelsAuto);
     view->setChannelsAuto(channelsAuto);
 
     auto stimulusAuto = settings.value(GLB_CONTROLS_STIMULUS_AUTO_TAG).toBool();
+    appStatus->setStimulusAuto(stimulusAuto);
     view->setStimulusAuto(stimulusAuto);
 
     auto expandAuto = settings.value(GLB_CONTROLS_EXPAND_AUTO_TAG).toBool();
+    appStatus->setExpandAuto(expandAuto);
     view->setExpandAuto(expandAuto);
 
     auto plotDetailAuto = settings.value(GLB_CONTROLS_PLOT_DETAIL_AUTO_TAG).toBool();
