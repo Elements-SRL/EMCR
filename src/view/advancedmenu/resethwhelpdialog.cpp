bee1b69c655ab0971032a735dc2f144191a91356
Nicola Arpino <nicolatosh4@gmail.com>
2026-06-22 16:20:12 +0200

    Updated reset help dialog with HTML version of the message.

--- src/view/advancedmenu/resethwhelpdialog.cpp
+++ src/view/advancedmenu/resethwhelpdialog.cpp
@@ -5,19 +5,33 @@ ResetHwHelpDialog::ResetHwHelpDialog(QWidget* parent) :
     MessageDialog("HW reset usage", true, parent) {
 
     this->addMainText(
-                "The HW reset should be used if the device seems stuck in saturation.\n"
-                "Before using the HW reset feature please check that the saturation is\n"
-                "actually due to a front end problem."
-                "In order to do so check that:\n"
-                "- you are using a proper current range in Voltage clamp; increase the current\n"
-                "  range and zoom out vertically to verify that the current trace is always stuck\n"
-                "  on the top or bottom value of the selected range;\n"
-                "- the resistance of your DUT is not too low in voltage clamp or too high in current\n"
-                "  clamp; in particular short circuits always saturate the voltage clamp front end\n"
-                "  and open circuits always saturate the current clamp front end;\n"
-                "- you are using a proper zoom to visualize the traces; in order to completely reset\n"
-                "  the zoom and center it around the traces double right click on any plot and click\n"
-                "  the Full trace zoom in the higher left corner of any plot window.");
+        "<p style='margin-bottom: 10px; font-size: 11px; font-weight: bold;'>"
+        "⚠️ The HW reset should be used if the device seems stuck in saturation."
+        "</p>"
+
+        "<p style='margin-bottom: 12px;'>"
+        "Before using the HW reset feature please check that the saturation is "
+        "actually due to a front end problem. In order to do so check that:"
+        "</p>"
+
+        "<ul style='margin-left: 12px;'>"
+        "  <li style='margin-bottom: 6px;'>"
+        "    <b>Voltage clamp range:</b> You are using a proper current range in Voltage clamp; "
+        "    increase the current range and zoom out vertically to verify that the current trace "
+        "    is always stuck on the top or bottom value of the selected range."
+        "  </li>"
+        "  <li style='margin-bottom: 6px;'>"
+        "    <b>DUT Resistance:</b> The resistance of your DUT is not too low in voltage clamp "
+        "    or too high in current clamp; in particular short circuits always saturate the "
+        "    voltage clamp front end and open circuits always saturate the current clamp front end."
+        "  </li>"
+        "  <li style='margin-bottom: 6px;'>"
+        "    <b>Traces Zoom:</b> You are using a proper zoom to visualize the traces; in order to "
+        "    completely reset the zoom and center it around the traces, <b>double right-click</b> on "
+        "    any plot and click the <i>Full trace zoom</i> in the higher left corner of any plot window."
+        "  </li>"
+        "</ul>"
+        );
 
     this->addDefaultButtonBox();
 
